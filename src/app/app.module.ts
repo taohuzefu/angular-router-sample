2055c3b3305f205be2ebf64c2946b52183ead82d
taohuzefu <taohuzefu@ilegend.cn>
2019-05-27 20:53:36 +0800

    step one

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,32 @@
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CrisisListComponent } from './crisis-list/crisis-list.component';
+import { HeroListComponent } from './hero-list/hero-list.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes : Routes = [
+  {path: 'crisis-center',component:CrisisListComponent},
+  {path: 'heroes',component:HeroListComponent},
+  { path: '',   redirectTo: '/heroes', pathMatch: 'full' },
+  {path: '**',component:PageNotFoundComponent},
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CrisisListComponent,
+    HeroListComponent,
+    PageNotFoundComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
